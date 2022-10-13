64873eb242db81b9e4bf8cde4c0fc27e4f596933
kurs22 <kurs22@Ivan-PC.dex.com>
2022-10-13 19:23:18 +0300

    task-15-feat: add test method

--- ServiceTests/CashDispenserServiceTests.cs
+++ ServiceTests/CashDispenserServiceTests.cs
@@ -11,7 +11,6 @@ public class CashDispenserServiceTests
         //arrange
         var bankContext = new BankContext();
         var token = new CancellationTokenSource();
-
         //act assert
         foreach (var task in WorkWithListTask(10, token, bankContext))
         {
@@ -21,10 +20,46 @@ public class CashDispenserServiceTests
         token.Cancel();
     }
 
+    [Test]
+    public async Task Test()
+    {
+        ThreadPool.SetMaxThreads(10, 10);
+        ThreadPool.GetAvailableThreads(out var worker, out var completition);
+        Console.WriteLine(worker.ToString());
+        var token = new CancellationTokenSource();
+        var listTask = new List<Task>();
+        for (int i = 0; i < 5; i++)
+        {
+            listTask.Add( Test2(token, worker));
+            Console.WriteLine($"{worker.ToString()}");
+            Task.Delay(1000).Wait();
+        }
+    }
+
+    private  Task Test2(CancellationTokenSource token, int worker)
+    {
+        return  Task.Run(() =>
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if (true)
+                {
+                    //Task.Delay(5000).Wait();
+                }
+
+                Console.WriteLine($"{worker.ToString()}");
+            }
+        });
+    }
+
+
     private List<Task> WorkWithListTask(int countTask, CancellationTokenSource token, BankContext bankContext)
     {
         var accountsDbs = bankContext.Accounts.Take(10).ToList();
         var listTask = new List<Task>();
+        ThreadPool.SetMaxThreads(10, 10);
+        ThreadPool.GetAvailableThreads(out var worker, out var completition);
+
         for (int i = 0; i < countTask; i++)
         {
             foreach (var accountDb in accountsDbs)
