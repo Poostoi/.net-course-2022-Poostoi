9f0fb41a2966fdfe477c3d803671277e62168a5e
kurs22 <kurs22@Ivan-PC.dex.com>
2022-10-13 17:19:05 +0300

    task-14-fix: making changes

---
+++ Services/RateUpdaterService.cs
@@ -0,0 +1,27 @@
+﻿using Migration;
+
+namespace Services;
+
+public class RateUpdaterService
+{
+    public Task UpdateRate(CancellationTokenSource token, BankContext bankContext)
+    {
+        var clientService = new ClientService(bankContext);
+        var convertService = new ConvertService();
+        return Task.Run(() =>
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var accountsDb = bankContext.Accounts.Take(10).ToList();
+
+                foreach (var accountDb in accountsDb)
+                {
+                    accountDb.Amount += 5;
+                    clientService.UpdateAccount(convertService.ConvertAccountDbInAccount(accountDb));
+                }
+
+                Task.Delay(5000).Wait();
+            }
+        });
+    }
+}
