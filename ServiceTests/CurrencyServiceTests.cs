c6510c7e7826db291eb3baca907dbe209750c6b0
Poostoi <oleg.nour@bk.ru>
2022-10-23 15:04:23 +0300

    task-17-feat: add class Reponse, CurrencyService and tsts

---
+++ ServiceTests/CurrencyServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Services;
+
+namespace ServiceTests;
+
+public class CurrencyServiceTests
+{
+    [Test]
+    public async Task AddClient_Client_AgeLessException()
+    {
+        //arrange
+        var dataGenerator = new TestDataGenerator();
+        var currency = dataGenerator.GeneratingCurrency();
+        var currencyService = new CurrencyService();
+        //act
+        var response = await currencyService.ConvertCurrency(currency);
+        //assert 
+        
+        Assert.NotNull(response);
+    }
+}
---
+++ Services/CurrencyService.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+using Newtonsoft.Json;
+namespace Services;
+
+public class CurrencyService
+{
+    public async Task<Response> ConvertCurrency(Currency currency)
+    {
+        Response response;
+        using (var client = new HttpClient())
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(
+                "https://www.amdoren.com/api/currency.php?api_key=j37SFVN4TCBV8H7pLPm2WpkVYSFTDY&from=EUR&to=GBP&amount=50");
+            responseMessage.EnsureSuccessStatusCode();
+
+            var message = await responseMessage.Content.ReadAsStringAsync();
+            response = JsonConvert.DeserializeObject<Response>(message);
+        }
+
+        return response;
+    }
+}
